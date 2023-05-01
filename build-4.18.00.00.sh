c840f7d9a31bc74b83f3c4128ea6f11a46c1cc84
Robert Nelson <robertcnelson@gmail.com>
2023-05-01 15:58:59 -0500

    a few more
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build-4.18.00.00.sh
+++ build-4.18.00.00.sh
@@ -14,14 +14,14 @@ if [ -f .builddir ] ; then
 	patch -p1 < ../../0001-debian-fno-PIE.patch
 	cd ../
 
-	if [ "x${ARCH}" = "xarmv7l" ] ; then
-		make_options="TOOLCHAIN_PREFIX= KERNEL_INSTALL_DIR=/build/buildd/linux-src release"
-	else
+	if [ "x${ARCH}" = "xx86_64" ] ; then
 		x86_dir="`pwd`/../../normal"
 		if [ -f `pwd`/../../normal/.CC ] ; then
 			. `pwd`/../../normal/.CC
 			make_options="TOOLCHAIN_PREFIX=${CC} KERNEL_INSTALL_DIR=${x86_dir}/KERNEL release"
 		fi
+	else
+		make_options="TOOLCHAIN_PREFIX= KERNEL_INSTALL_DIR=/build/buildd/linux-src release"
 	fi
 
 	cd ./src/src/cmem/module/
